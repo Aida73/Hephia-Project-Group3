9410e3428b5197dde64e32f0a88aefacd8e40d1e
Aida Sow <sowaida@ept.sn>
2024-02-21 21:42:31 +0100

    add test.py

---
+++ DataMasking/pi_data_masking.py
@@ -0,0 +1,64 @@
+from modal import Stub, web_endpoint, Image
+import spacy
+import re
+import random
+import string
+from pydantic import BaseModel
+
+image = Image.debian_slim().pip_install("spacy").run_commands(
+        "python -m spacy download 'en_core_web_sm'"
+    )
+stub = Stub("heph3-pi-mask",image=image)
+
+
+nlp = spacy.load("en_core_web_sm")
+
+def generate_random_string(length):
+    # Générer une chaîne de caractères aléatoires de la même longueur
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def anonymize_phone_numbers(text):
+    return re.sub(r'\b\d{2} \d{2} \d{2} \d{2} \d{2}\b', 'XX-XXX-XX-XX', text)
+
+def anonymize_emails(text):
+    return re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', 'anonyme@anonyme.com', text)
+
+def anonymize_nss(text):
+    text = re.sub(r'\b\d{1} \d{2} \d{2} \d{2} \d{3} \d{3} \d{2}\b', 'a certain value', text)
+    return re.sub(r'\b\d{15}\b', 'a certain value', text)
+
+def anonymize_passport_visa(text):
+    return re.sub(r'\b[A-Z]{1}[0-9]{8}\b', "a certain value", text)
+
+def anonymize_entities(text):
+    doc = nlp(text)
+    replacements = {
+        "PERSON": "Name",
+        "ORG": "Organization",
+        "NORP": "Group",
+        "GPE": "Location",
+        "FAC": "Facility",
+    }
+    anonymized_text = text
+    for ent in reversed(doc.ents):
+        if ent.label_ in replacements:
+            random_string = generate_random_string(len(ent.text))
+            anonymized_text = anonymized_text[:ent.start_char] + random_string + anonymized_text[ent.end_char:]
+    return anonymized_text
+
+
+class Data(BaseModel):
+    text: str
+
+@stub.function(gpu="h100")
+@web_endpoint(method="POST")
+def anonymize(data: Data):
+    text = data.text
+    text = anonymize_entities(text)
+    text = anonymize_phone_numbers(text)
+    text = anonymize_emails(text)
+    text = anonymize_nss(text)
+    text = anonymize_passport_visa(text)
+    return {"anonymized_text": text}
---
+++ DataMasking/script2.py
@@ -0,0 +1,58 @@
+import spacy
+import re
+import random
+import string
+
+
+nlp = spacy.load("en_core_web_sm")
+
+def generate_random_string(length):
+    # Générer une chaîne de caractères aléatoires de la même longueur
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def anonymize_phone_numbers(text):
+    return re.sub(r'\b\d{2} \d{2} \d{2} \d{2} \d{2}\b', 'XX-XXX-XX-XX', text)
+
+def anonymize_emails(text):
+    return re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', 'anonyme@anonyme.com', text)
+
+def anonymize_nss(text):
+    text = re.sub(r'\b\d{1} \d{2} \d{2} \d{2} \d{3} \d{3} \d{2}\b', 'a certain value', text)
+    return re.sub(r'\b\d{15}\b', 'a certain value', text)
+
+def anonymize_passport_visa(text):
+    return re.sub(r'\b[A-Z]{1}[0-9]{8}\b', "a certain value", text)
+
+def anonymize_entities(text):
+    doc = nlp(text)
+    replacements = {
+        "PERSON": "Name",
+        "ORG": "Organization",
+        "NORP": "Group",
+        "GPE": "Location",
+        "FAC": "Facility",
+    }
+    anonymized_text = text
+    for ent in reversed(doc.ents):
+        if ent.label_ in replacements:
+            random_string = generate_random_string(len(ent.text))
+            anonymized_text = anonymized_text[:ent.start_char] + random_string + anonymized_text[ent.end_char:]
+    return anonymized_text
+
+def anonymize(text):
+    text = anonymize_entities(text)
+    text = anonymize_phone_numbers(text)
+    text = anonymize_emails(text)
+    text = anonymize_nss(text)
+    text = anonymize_passport_visa(text)
+    return text
+
+original_text = """Hello Jean Dupont, your telephone number is 06 12 34 56 78 and your email address 
+is aida.sow@example.com. I'm muslim.I live in Bois d'arcy. I work for Orange France I'm currently 
+at 1 Avenue Pierrefitte-Sur Seinee. I'm 12. My NSS is 2 99 03 99 432 123 32. My passport number is A02134432"""
+
+anonymized_text = anonymize(original_text)
+print("Texte anonymisé :\n", anonymized_text)
+
